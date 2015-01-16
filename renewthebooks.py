f08704e8c92173525a6da10ad6e06591ece50c5a
truehyp <truehyp@gmail.com>
2015-01-16 20:40:26 +0800

    添加导出借阅历史功能

--- renewthebooks.py
+++ renewthebooks.py
@@ -1,8 +1,9 @@
 ##############################
 #
-#时间：2015-01-15
+#时间：2015-01-16
 #作者： truehyp
 #说明：程序能自动续借用户所有书籍，续借后，借阅时间变为当前时间，应还时间为借阅时间加续借的天数（默认为30)
+#并有导出借阅历史的功能
 #
 ##############################
 import urllib
@@ -10,6 +11,44 @@ import urllib.request
 import http.cookiejar
 import re
 import getpass
+import os
+
+
+def renew_history():
+    #读者借阅历史
+    data2=b'dzhm='+name.encode('gb2312')+b'&cxsj1=2011-01-01&cxsj2=2015-06-01&submit=%B6%C1%D5%DF%BD%E8%D4%C4%C0%FA%CA%B7'
+    ls_request=urllib.request.Request('http://lib.cjlu.edu.cn/ttweb/dzjyls.php',data2)
+    response=opener.open(ls_request)
+    #win7的cmd默认为gbk编码，用ignore参数忽略gbk无法编码的字符
+    ans=response.read().decode('gbk','ignore')
+    #正则表达式取值
+    bookitem=re.findall(r'<tr><td>(.*?)</td>.*?<td>.*?</td>.*?<td>.*?</td>.*?<td>(.*?)</td>.*?<td>(.*?)</td></tr>',ans,re.S)
+    #导出到文件
+    fp=open('book.txt','w')
+    for item in bookitem:
+        fp.write(str(item[0])+' '+str(item[1])+' '+str(item[2]+'\n'))
+
+
+def renew_book(ans):
+	#匹配书的编号
+	match_number=re.findall(r'<tr><td>.*?</td>.*?<td>.*?</td>.*?<td>(.*?)</td>.*?</tr>',ans,re.S)
+	#生成postdata
+	data1=b'T1='+name.encode('gb2312')+b'&Submit=%d0%f8%bd%e8%d1%a1%d6%d0%cd%bc%ca%e9'
+	for item in match_number:
+		data1=b'netxj%5b%5d='+item.encode('gb2312')+b'%2f%2f30%2f%2f-1&'+data1
+	#续借信息
+	print ('正在续借所有书籍...')
+	renew_request=urllib.request.Request('http://lib.cjlu.edu.cn/ttweb/bkxj1.php',data1)
+	#进行续借操作
+	response=opener.open(renew_request)
+	#打印续借返回页面
+	ans=response.read().decode('gb2312')
+	###加一个返回信息，提示续借结果
+	if re.findall(r'成功',ans):
+		print('成功续借')
+	else:
+		print('续借失败')
+	input("按任意键退出: ")
 
 
 #获取用户名和密码，并且组成postdata
@@ -22,9 +61,10 @@ login_request=urllib.request.Request('http://lib.cjlu.edu.cn/ttweb/dz.php',data)
 #设置cookie
 cookie=http.cookiejar.CookieJar()
 opener=urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
+
 #登陆
 response=opener.open(login_request)
-#登陆返回页面
+#登陆结果
 ans=response.read().decode('gb2312')
 
 if re.findall(r'没有该读者',ans):
@@ -36,25 +76,11 @@ if re.findall(r'重新输入',ans):
 	print('输入读者号和密码')
 	input("按任意键退出: ")
 	exit()
-
-#匹配书的编号
-match_number=re.findall(r'<tr><td>.*?</td>.*?<td>.*?</td>.*?<td>(.*?)</td>.*?</tr>',ans,re.S)
-
-#生成postdata
-data1=b'T1='+name.encode('gb2312')+b'&Submit=%d0%f8%bd%e8%d1%a1%d6%d0%cd%bc%ca%e9'
-for item in match_number:
-    data1=b'netxj%5b%5d='+item.encode('gb2312')+b'%2f%2f30%2f%2f-1&'+data1
-#续借信息
-print ('正在续借所有书籍...')
-xj_request=urllib.request.Request('http://lib.cjlu.edu.cn/ttweb/bkxj1.php',data1)
-#进行续借操作
-response=opener.open(xj_request)
-#续借返回页面
-ans=response.read().decode('gb2312')
-###加一个返回信息，提示续借结果
-if re.findall(r'成功',ans):
-    print('成功续借')
+if input('回车直接续借图书\n空格再回车导出图书借阅历史: ')==' ':
+	print ('正在导出...')
+	renew_history()
+	print ('借阅历史已导出到'+os.getcwd()+os.sep+'book.txt')
+	exit()
 else:
-    print('续借失败')
-input("按任意键退出: ")
-exit()
+	renew_book(ans)
+	exit()
