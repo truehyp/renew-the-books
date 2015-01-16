4f74769be735c9884fca122d72dcc8ac5b6961fb
侯爷 <truehyp@gmail.com>
2015-01-16 09:05:10 +0800

    Update renewthebooks.py

--- renewthebooks.py
+++ renewthebooks.py
@@ -1,24 +1,31 @@
+##############################
+#
+#学校图书馆网址：http://lib.cjlu.edu.cn
+#时间：2015-01-15
+#作者： truehyp
+#说明：程序能自动续借用户所有书籍，续借后，借阅时间变为当前时间，应还时间为借阅时间加续借的天数（默认为30)
+#
+##############################
 import urllib
 import urllib.request
 import http.cookiejar
 import re
 import getpass
 
-#续借后，借阅时间为当前时间，应还时间为借阅时间加续借的天数（默认为30)
+
 #获取用户名和密码，并且组成postdata
 name = input('username: ')
 pwd = getpass.getpass('password: ')
 data=b'T1='+name.encode('gb2312')+b'&xm=&tday1=26360&B1=%b5%c7%c2%bd&T3='+pwd.encode('gb2312')
 
 #登陆信息
-#data=b'T1=1100801316&xm=&tday1=26360&B1=%b5%c7%c2%bd&T3=921913'
 login_request=urllib.request.Request('http://lib.cjlu.edu.cn/ttweb/dz.php',data)
 #设置cookie
 cookie=http.cookiejar.CookieJar()
 opener=urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
 #登陆
 response=opener.open(login_request)
-#登陆结果
+#登陆返回页面
 ans=response.read().decode('gb2312')
 
 if re.findall(r'没有该读者',ans):
@@ -40,11 +47,10 @@ for item in match_number:
     data1=b'netxj%5b%5d='+item.encode('gb2312')+b'%2f%2f30%2f%2f-1&'+data1
 #续借信息
 print ('正在续借所有书籍...')
-#data1=b'netxj%5b%5d=00992810%2F%2F5%2F%2F-1&T1=1100801316&Submit=%d0%f8%bd%e8%d1%a1%d6%d0%cd%bc%ca%e9'
 xj_request=urllib.request.Request('http://lib.cjlu.edu.cn/ttweb/bkxj1.php',data1)
 #进行续借操作
 response=opener.open(xj_request)
-#打印续借返回页面
+#续借返回页面
 ans=response.read().decode('gb2312')
 ###加一个返回信息，提示续借结果
 if re.findall(r'成功',ans):
